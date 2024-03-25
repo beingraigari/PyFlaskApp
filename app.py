5aa1e9761c5252ab7607ffdba0d637f96f7bb6ec
beingraigari <beingraigari@gmail.com>
2024-03-25 19:15:45 +0530

    First iteration of the app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "beingraigari_0321"
+
+@app.route("/hello")
+def index():
+	flash("Please enter your first and last name.")
+	return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['firstname_input']) + " " + str(request.form['secondname_input']))
+	return render_template("index.html")
