34c16c25a04449eeb02003c619679935bd424352
beingraigari <beingraigari@gmail.com>
2024-03-25 19:54:00 +0530

    Changed the route to /

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "beingraigari_0321"
 
-@app.route("/hello")
+@app.route("/")
 def index():
 	flash("Please enter your first and last name.")
 	return render_template("index.html")
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
-	flash("Hi " + str(request.form['firstname_input']) + " " + str(request.form['secondname_input']))
+	flash("Hello " + str(request.form['firstname_input']) + " " + str(request.form['secondname_input']))
 	return render_template("index.html")
