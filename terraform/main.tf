592570bfbfd355b1351962c3f8a70cc4f0de0172
beingraigari <beingraigari@gmail.com>
2024-04-09 12:16:17 +0530

    Modified deploy.yml to enable monitoring

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,6 +94,17 @@ resource "aws_security_group" "maingroup" {
       security_groups  = []
       self             = false
       to_port          = 80
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 9100
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 9100
     }
   ]
 }
