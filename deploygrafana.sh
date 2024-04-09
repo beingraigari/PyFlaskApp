28e1704436320224ff6bccf113f6f98dd0f606af
beingraigari <beingraigari@gmail.com>
2024-04-09 14:17:48 +0530

    Modified deploy.yml to install grafana

---
+++ deploygrafana.sh
@@ -0,0 +1,16 @@
+sudo apt-get install -y adduser libfontconfig1 musl
+
+wget https://dl.grafana.com/oss/release/grafana_10.4.1_amd64.deb
+
+sudo dpkg -i grafana_10.4.1_amd64.deb
+
+sudo /bin/systemctl daemon-reload
+
+sudo /bin/systemctl enable grafana-server
+
+sudo /bin/systemctl start grafana-server
+
+sudo systemctl enable grafana-server.service
+
+
+
