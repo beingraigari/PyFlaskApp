bc5de0b1ee3c44fb46ebbb726b22827e4aa15821
raigari <beingraigari@gmail.com>
2024-10-09 18:48:15 +0530

    made a few changes

--- deployprom.sh
+++ deployprom.sh
@@ -1,6 +1,11 @@
 echo "deleting old prom app"
 sudo rm -rf Prometheus_Monitoring/
 
+
+sudo kill -9 `sudo lsof /usr/local/bin/prometheus`
+
+
+
 sudo git clone https://github.com/vipinkumar1234/Prometheus_Monitoring.git
 
 cd Prometheus_Monitoring/
